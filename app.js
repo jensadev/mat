6e64e7c169c4a11ef0060f1af69efd2208d0bcd5
jensnti <jens.andreasson@ga.ntig.se>
2021-02-14 13:44:54 +0100

    feat: adds cors
    feat: frontend test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 require('dotenv').config({path: './.env'});
 
@@ -9,6 +10,7 @@ const mealRouter = require('./routes/meals');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
