4404d4e443a964f9a0f6ef3e30857c8b73fe2768
jensnti <jens.andreasson@ga.ntig.se>
2021-02-14 13:08:33 +0100

    refactor(mealcontroller): json errors response, validation
    fix: adds date from request, not on post

--- controllers/MealController.js
+++ controllers/MealController.js
@@ -14,13 +14,13 @@ module.exports.show = async (req, res) => {
 
   let id = parseInt(req.params.id);
   if (!Number.isInteger(id)) {
-    return res.status(400).send('Invalid id type');
+    return res.status(400).json({ errors: 'Invalid id'});
   }
 
   let meal = await Meal.find(id);
 
   if (!meal) {
-    return res.status(404).send('Object not found');
+    return res.status(404).json({ errors: 'Object not found' });
   }
   return res.send(meal);
 };
@@ -34,11 +34,11 @@ module.exports.store = async (req, res) => {
   let meal = new Meal({
     name: req.body.name,
     type_id: req.body.type_id,
-    date: new Date().toISOString().split('T')[0]
+    date:  req.body.date
   });
   const result = await meal.save();
   if (result == 0) {
-    return res.status(400).send('Invalid request');
+    return res.status(400).json({ errors: 'Invalid request' });
   }
 
   return res.send(meal);
@@ -49,15 +49,15 @@ module.exports.update = async (req, res) => {
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  
+
   let id = parseInt(req.params.id);
   let meal = await Meal.find(id);
   if (typeof meal === 'undefined') {
-    return res.status(500).send('Error: Meal undefined');
+    return res.status(400).json({ errors: 'Invalid request' });
   }
   const result = await meal.save(req.body);
   if (result == 0) {
-    return res.status(400).send('Invalid request');
+    return res.status(400).json({ errors: 'Invalid request' });
   }
   return res.send(meal);
 };
@@ -66,7 +66,7 @@ module.exports.destroy = async (req, res) => {
   let id = parseInt(req.params.id);
   const result = await Meal.delete(id);
   if (result == 0) {
-    return res.status(400).send('Invalid request');
+    return res.status(400).json({ errors: 'Invalid request' });
   }
   return res.send('Meal deleted');
 };
