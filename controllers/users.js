0ebf64d7eb5faa4e2345cd420237d32bef19076e
jensnti <jens.andreasson@ga.ntig.se>
2021-03-21 10:23:58 +0100

    feat: pagination now works with orm for fetching meals

--- controllers/users.js
+++ controllers/users.js
@@ -2,13 +2,11 @@ const { Meal, User, Dish, Mealtype, User_Dish } = require('../models/');
 
 const adjektiv = require('../docs/adjektiv.json');
 const substantiv = require('../docs/substantiv.json');
-
+const paginate = require('jw-paginate');
 const validator = require('express-validator');
 
 module.exports.meals = async (req, res) => {
   try {
-    console.table(req.user.sub);
-
     const user = await User.findOne({ where: { sub: splitSub(req.user.sub) } });
     // const user = await User.findOne({ where: { id: 1 } });
     if (!user) {
@@ -22,61 +20,68 @@ module.exports.meals = async (req, res) => {
         {
           model: Dish
           // where: { name: 'Dish name'}
-        },
-        {
-          model: Mealtype
         }
       ]
     });
 
     const meals = [];
-    if (getMeals)
+    if (getMeals) {
       for (let meal of getMeals) {
         meals.push(meal.dataValues);
       }
-    res.status(200).json({ meals });
+    }
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = 10;
+    const pager = paginate(meals.length, page, pageSize);
+    const pageOfItems = meals.slice(pager.startIndex, pager.endIndex + 1);
+
+    res.status(200).json({pager, pageOfItems});
   } catch (e) {
     res.status(422).json({ errors: { body: [e.message] } });
   }
 };
 
 module.exports.dishes = async (req, res) => {
-  try {
-    console.table(req.user.sub);
+  const dishes = [
+    {id: 1, name: 'Mat'},
+    {id: 2, name: 'Fläsk'}
+  ]
+  res.status(200).json({ dishes });
+  // try {
+  //   const user = await User.findOne({ where: { sub: splitSub(req.user.sub) } });
+  //   // const user = await User.findOne({ where: { id: 1 } });
+  //   if (!user) {
+  //     res.status(404);
+  //     throw new Error('User id not valid');
+  //   }
 
-    const user = await User.findOne({ where: { sub: splitSub(req.user.sub) } });
-    // const user = await User.findOne({ where: { id: 1 } });
-    if (!user) {
-      res.status(404);
-      throw new Error('User id not valid');
-    }
+  //   const getDishes = await User_Dish.findAll({
+  //     where: { userId: user.id },
+  //     include: [
+  //       {
+  //         model: Dish
+  //         // where: { name: 'Dish name'}
+  //       }
+  //     ]
+  //   });
 
-    const getDishes = await User_Dish.findAll({
-      where: { userId: user.id },
-      include: [
-        {
-          model: Dish
-          // where: { name: 'Dish name'}
-        }
-      ]
-    });
-
-    const dishes = [];
-    if (getDishes)
-      for (let dish of getDishes) {
-        dishes.push(dish.dataValues.Dish);
-      }
-    res.status(200).json({ dishes });
-  } catch (e) {
-    res.status(422).json({ errors: { body: [e.message] } });
-  }
+  //   const dishes = [];
+  //   if (getDishes)
+  //     for (let dish of getDishes) {
+  //       dishes.push(dish.dataValues.Dish);
+  //     }
+  //   res.status(200).json({ dishes });
+  // } catch (e) {
+  //   res.status(422).json({ errors: { body: [e.message] } });
+  // }
 };
 
 module.exports.store = async (req, res) => {
   try {
     if (!req.user.sub) throw new Error('Sub is Required');
     if (!req.body.email) throw new Error('Email is Required');
-    if (!validator.isEmail(req.body.email)) throw new Error('Email is Required');
+    if (!validator.isEmail(req.body.email))
+      throw new Error('Email is Required');
     // console.table(req.user);
     // console.table(req.body.email);
 
