bc287e174dc67dcc824d8b6b0e23c9a39a8bb1e7
jensnti <jens.andreasson@ga.ntig.se>
2021-04-20 10:33:06 +0200

    fix: ?

--- config/config.js
+++ config/config.js
@@ -12,6 +12,11 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "dialectOptions": {
+        "ssl": {
+            "rejectUnauthorized": false
+        }
+    }
   }
 };
